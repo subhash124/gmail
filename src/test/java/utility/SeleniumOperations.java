0ef4b0ddea6fb69ced8fb492f4ba55efa83e0521
subhash124 <subhashhogale5@gmail.com>
2023-05-07 14:02:32 +0530

    shailaja commit 1

--- src/test/java/utility/SeleniumOperations.java
+++ src/test/java/utility/SeleniumOperations.java
@@ -18,7 +18,7 @@ public class SeleniumOperations
 		driver=new ChromeDriver(option);
 		driver.manage().window().maximize();
 		
-		
+		System.out.println("hello");
 	}
 	public static void openurl(String str) {
 		driver.get(str);
@@ -33,6 +33,6 @@ public class SeleniumOperations
 		
 		driver.findElement(By.xpath(str)).sendKeys(str1);
 	}
-	
+
 
 }
